2e67cfad2538481979ea424f99cd53694c481d49
Brendan Reardon <9423424+brendanreardon@users.noreply.github.com>
2025-05-25 23:48:42 -0400

    Removed print statement

--- run.py
+++ run.py
@@ -11,6 +11,5 @@ if __name__ == '__main__':
     host = os.environ.get('FLASK_HOST', '0.0.0.0')
     port = int(os.environ.get('FLASK_PORT', 5000))
     debug = bool(os.environ.get('FLASK_DEBUG', False))
-    print(host, port, debug, type(debug))
 
     app.run(host=host, port=port, debug=debug)
